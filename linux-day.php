89dbe7e799e10d2ece8fd5a17ac673dd94eeb773
deshack <deshack@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-14 11:07:56 +0000

    Add new banners for Linux Day 2015
    git-svn-id: https://plugins.svn.wordpress.org/linux-day/tags/1.2.0@1198654 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ linux-day.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Plugin Name: Linux Day
+ * Plugin URI: https://github.com/deshack/linux-day
+ * Description: This plugin adds a widget for Linux Day banners.
+ * Author: Mattia Migliorini
+ * Version: 1.2.0
+ * Author URI: http://www.deshack.net
+ * License: GPLv2 or later
+ */
+
+// Load Text Domain
+function ld_text_start() {
+	load_plugin_textdomain( 'ld', false, 'linux-day/languages' );
+}
+add_action( 'init', 'ld_text_start' );
+
+/**
+ * Linux Day Italy Widget Class
+ */
+class ld_widget_banner extends WP_Widget {
+
+	// Constructor
+	function __construct() {
+		parent::__construct(
+			'ld_widget_banner', // Base ID
+			__( 'Linux Day Banner', 'ld' ), // Name
+			array( 'description' => __( 'Add the Linux Day banners to your sidebar', 'ld' ) ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args. Widget arguments.
+	 * @param array $instance. Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$format = $instance['format'];
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+		if ( ! empty( $format ) ) :
+		?>
+		<a href="http://www.linuxday.it">
+			<?php $banner = 'images/banner_' . $format . '.png'; ?>
+			<img src="<?php echo plugins_url( $banner, __FILE__ ); ?>" border="0" alt="Linux Day 2015">
+		</a>
+		<?php
+		endif;
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance. Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$sizes = array( '180x150', '300x250', '468x60', '120x600' );
+		$selected = 'selected="selected"';
+		$title = isset($instance['title']) ? $instance['title'] : '';
+		$format = isset($instance['format']) ? $instance['format'] : '';
+		?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:', 'ld' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('format'); ?>"><?php _e( 'Banner format:', 'ld' ); ?></label>
+			<select class="widefat" id="<?php echo $this->get_field_id('format'); ?>" name="<?php echo $this->get_field_name('format'); ?>">
+				<option <?php selected( $format, '' ); ?>><?php _e( 'Select format', 'ld' ); ?></option>
+				<?php foreach( $sizes as $size ) : ?>
+					<option <?php selected( $size, $format ); ?> value="<?php echo esc_attr($size); ?>"><?php echo $size; ?></option>
+				<?php endforeach; ?>
+			</select>
+		</p>
+
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance. Values just sent to be saved.
+	 * @param array $old_instance. Previously saved values from database.
+	 *
+	 * @return array. Updated safe values to be saved.
+	 */
+	public function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = $new_instance['title'];
+		$instance['format'] = $new_instance['format'];
+
+		return $instance;
+	}
+}
+
+function ld_register_widget() {
+	register_widget( 'ld_widget_banner' );
+}
+add_action( 'widgets_init', 'ld_register_widget' );
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Linux Day ===
+Contributors: deshack
+Donate link: http://www.deshack.net/donate/
+Tags: widget, linux-day, linux, banner
+Requires at least: 2.8
+Tested up to: 4.3
+Stable tag: 1.2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+
+This plugin adds a widget for Linux Day banners.
+
+== Description ==
+
+Simply add Linux Day banners to your sidebar with a handy widget.
+
+== Installation ==
+
+1. Download and extract the plugin's archive.
+2. Put the extracted folder in the `/wp-content/plugins/` directory.
+3. Activate the plugin in the WordPress Admin.
+
+Or simply install it from the WP Admin Plugins section.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.2.0 =
+* Add new banners for Linux Day 2015.
+
+= 1.1.0 =
+* Add new banners for the Linux Day 2014.
+
+= 1.0.1 =
+* Fix widget form notices.
+* Compatible up to WP 3.9.1
+
+= 1.0.0 =
+* Feature: 4 banner formats
+* Feature: Italian translation
+
+== Upgrade notice ==
+
+Introduced the new banners of 2014.
